cf91548516edd17378a6079ad37901f12b3383f2
Пономарев Денис <desa-196@yandex.ru>
2023-12-28 10:24:23 +0300

    Создал классы Animal и Comman

---
+++ AnymalsPROG/AnymalsPROG/Model/Animals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnymalsPROG.Model
+{
+    internal abstract class Animals
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime birthday { get; set; }
+        public string color {  get; set; }
+        public List<Command> commands { get; set; }
+        public void LernCommand(Command command) 
+        {
+            commands.Add(command);
+        }
+    }
+}
---
+++ AnymalsPROG/AnymalsPROG/Model/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnymalsPROG.Model
+{
+    internal class Command
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public void Run()
+        {
+            System.Windows.MessageBox.Show(name);
+        }
+    }
+}
