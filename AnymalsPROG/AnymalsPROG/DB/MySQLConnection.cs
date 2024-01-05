624a0c93f06418c4177e3ebbe252ead0474aab74
Пономарев Денис <desa-196@yandex.ru>
2024-01-05 17:11:34 +0300

    Сделал вывод через datagrid

--- AnymalsPROG/AnymalsPROG/DB/MySQLConnection.cs
+++ AnymalsPROG/AnymalsPROG/DB/MySQLConnection.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using System.Windows.Media.Media3D;
 using System.Xml.Linq;
 
 namespace AnymalsPROG.DB
@@ -27,7 +28,24 @@ namespace AnymalsPROG.DB
             {
                 connection.Open();
 
-                string sql = "select * from cats union select * from dogs union select * from donkeys union select * from camels union select * from hamsters union select * from horses";
+                string sql =
+                    "select all_animals.id, all_animals.Birthday, all_animals.name, colors.color " +
+                    "from("+
+                        "select * from cats "+
+                        "union " +
+                        "select * from dogs " +
+                        "union " +
+                        "select * from donkeys " +
+                        "union " +
+                        "select * from camels " +
+                        "union " +
+                        "select * from hamsters " +
+                        "union  " +
+                        "select * from horses) as all_animals " +
+                    "left join colors on all_animals.color = colors.id " +
+                    "left join anymals_types on all_animals.anymal_classes_id = anymals_types.id " +
+                    "left join anymal_classes on anymals_types.anymal_classes_id = anymal_classes.id";
+
                 using (MySqlCommand command = new MySqlCommand(sql, connection))
                 {
                     using (MySqlDataReader reader = command.ExecuteReader())
@@ -35,7 +53,7 @@ namespace AnymalsPROG.DB
                         while (reader.Read())
                         {
                             //AninalsList.Add(new Camel(reader["id"], reader["name"], reader["birthday"], reader["color"], null));
-                            AninalsList.Add(new Camel(1, reader["name"].ToString(), DateTime.Now, reader["color"].ToString(), null));
+                            AninalsList.Add(new Camel(int.Parse(reader["id"].ToString()), reader["name"].ToString(), DateTime.Parse(reader["birthday"].ToString()), reader["color"].ToString(), null));
                         }
                     }
                 }
